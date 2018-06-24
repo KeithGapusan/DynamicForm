c21ba52c8a247bd7dccdd89ba5a5a3ddef0998b9
keith31 <ksgapusan@straightarrow.com.ph>
2018-06-24 10:22:04 +0800

    add new test method

---
+++ DynamicForm/DynamicForm/DynamicForm.h
@@ -0,0 +1,19 @@
+//
+//  DynamicForm.h
+//  DynamicForm
+//
+//  Created by Keith Randell Gapusan on 24/06/2018.
+//  Copyright © 2018 Keith Randell Gapusan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicForm.
+FOUNDATION_EXPORT double DynamicFormVersionNumber;
+
+//! Project version string for DynamicForm.
+FOUNDATION_EXPORT const unsigned char DynamicFormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicForm/PublicHeader.h>
+
+
---
+++ DynamicForm/DynamicForm/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  DynamicForm
+//
+//  Created by Keith Randell Gapusan on 24/06/2018.
+//  Copyright © 2018 Keith Randell Gapusan. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service{
+    
+    private init(){}
+    public static func testService() -> String{
+        return "Successfully implemented"
+    }
+    
+}
